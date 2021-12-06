8b5774a2db08d6766d9736da4e79d2f99dea597b
BaryAll <baryshevyur7@gmail.com>
2021-12-06 19:20:03 +0200

    commit

--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -6,42 +6,44 @@ import houseForm from '../images/house-form.png';
 function FormContainer () {
     return (
         <div className="form-container">
-            <div>
+            <div className="lf__form-container">
                 <img src={houseForm} alt="house-form" />
             </div>
-            <form>
-                <h2>Contact us</h2>
-                <p>Turpis facilisis tempor pulvinar in lobortis ornare magna.</p>
-                <div>
-                    <input 
-                        placeholder="First name" 
-                        type="text" 
-                        name="first__name"
-                    />
-                    <input 
-                        placeholder="Last name" 
-                        type="text" 
-                        name="last__name" 
-                    />
-                </div>
-                <div>
-                    <input 
-                        placeholder="Emal address" 
-                        type="email" 
-                        name="email" 
-                    />
-                    <input 
-                        placeholder="Phone number" 
-                        type="text" 
-                        name="phone__number" 
-                    />
-                </div>
-                <textarea name="textarea" placeholder="Description"></textarea>
-                <div>
-                    <button>Get a quote</button>
-                    <p>We don't sell your email and spam.</p>
-                </div>
-            </form>
+            <div className="rg__form-container">
+                <form>
+                    <h2>Contact us</h2>
+                    <p>Turpis facilisis tempor pulvinar in lobortis ornare magna.</p>
+                    <div>
+                        <input 
+                            placeholder="First name" 
+                            type="text" 
+                            name="first__name"
+                        />
+                        <input 
+                            placeholder="Last name" 
+                            type="text" 
+                            name="last__name" 
+                        />
+                    </div>
+                    <div>
+                        <input 
+                            placeholder="Emal address" 
+                            type="email" 
+                            name="email" 
+                        />
+                        <input 
+                            placeholder="Phone number" 
+                            type="text" 
+                            name="phone__number" 
+                        />
+                    </div>
+                    <textarea name="textarea" placeholder="Description"></textarea>
+                    <div>
+                        <button>Get a quote</button>
+                        <p>We don't sell your email and spam.</p>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
