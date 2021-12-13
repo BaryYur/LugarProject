1f39289a873029ba806ba7e3edf4fe9fe3e67771
BaryAll <baryshevyur7@gmail.com>
2021-12-13 12:30:52 +0200

    commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,61 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
+import './Footer.css';
+ 
 function Footer() {
     return (
         <footer>
             <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div>
+                <p></p>
+                <ul>
+                    <li>
+                        <Link to="/"></Link>
+                    </li>
+                    <li>
+                        <Link to="/"></Link>
+                    </li>
+                    <li>
+                        <Link to="/"></Link>
+                    </li>
+                    <li>
+                        <Link to="/"></Link>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <p></p>
+                <ul>
+                    <li>
+                        <Link to="/"></Link>
+                    </li>
+                    <li>
+                        <Link to="/"></Link>
+                    </li>
+                    <li>
+                        <Link to="/"></Link>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <p></p>
+                <ul>
+                    <li>
+                        <Link to="/"></Link>
+                    </li>
+                    <li>
+                        <Link to="/"></Link>
+                    </li>
+                    <li>
+                        <Link to="/"></Link>
+                    </li>
+                    <li>
+                        <Link to="/"></Link>
+                    </li>
+                </ul>
+            </div>
         </footer>
     );
 };
