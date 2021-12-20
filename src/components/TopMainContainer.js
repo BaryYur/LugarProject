2d48a8a824931df99d5a46e7eb181eba14add98f
BaryYur <73163625+BaryYur@users.noreply.github.com>
2021-12-20 15:11:57 +0200

    commit

---
+++ src/components/TopMainContainer.js
@@ -0,0 +1,32 @@
+import topMainImg from '../images/first-main-img.png';
+import './TopMainContainer.css';
+
+function TopMainContainer() {
+    return (
+        <div className="top-main-container">
+                <div className="top-main__left-container">
+                    <img src={topMainImg} alt="first-house" />
+                </div>
+                <div className="top-main__right-container">
+                    <h2>Award winning real estate company in Dubai</h2>
+                    <p className="description">Semper arcu mauris aliquam lacus. Massa erat vitae ultrices pharetra scelerisque. Ipsum, turpis facilisis tempor pulvinar in lobortis ornare magna.</p>
+                    <div className="features-container">
+                        <div>
+                            <p>Previous projects</p>
+                            <p>34+</p>
+                        </div>
+                        <div>
+                            <p>years experience</p>
+                            <p>20y</p>
+                        </div>
+                        <div>
+                            <p>Ongoing projects</p>
+                            <p>12</p>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    );
+};
+
+export default TopMainContainer;
